40d64467107cb24ea0354f263a52b0c835b3b0f6
Simon <54853018+simonMarcotte@users.noreply.github.com>
2024-11-08 04:38:57 -0700

    added qr code generation and scanning capabilities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.api.dsl.Packaging
-
 plugins {
     alias(libs.plugins.android.application)
 
@@ -35,11 +33,6 @@ android {
     buildFeatures {
         viewBinding = true
     }
-    fun Packaging.() {
-        resources {
-            excludes += "mockito-extensions/org.mockito.plugins.MockMaker"
-        }
-    }
 
 }
 
@@ -63,28 +56,23 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.play.services.location)
     implementation(libs.firebase.storage)
-    implementation(libs.espresso.intents)
 //    implementation(libs.rules)
 //    implementation("androidx.test:rules:1.4.0")
     testImplementation(libs.junit)
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
     testImplementation("org.assertj:assertj-core:3.17.2")
-    testImplementation("org.mockito:mockito-inline:3.12.4")
+    testImplementation("org.mockito:mockito-core:3.5.13")
+
+    var camerax_version = "1.1.0" // Use the latest stable version if possible
+    implementation("androidx.camera:camera-core:$camerax_version")
+    implementation ("androidx.camera:camera-camera2:$camerax_version")
+    implementation ("androidx.camera:camera-lifecycle:$camerax_version")
+    implementation ("androidx.camera:camera-view:$camerax_version") // For PreviewView
+    implementation ("androidx.camera:camera-extensions:$camerax_version")
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.test.runner)
     androidTestImplementation(libs.rules)
-    // Mockito for Android Instrumented Tests
-//    androidTestImplementation("org.mockito:mockito-android:3.12.4")
-
-    // Dexmaker dependencies for mocking final classes
-//    androidTestImplementation("org.mockito:mockito-core:3.12.4") // Add this line
-    androidTestImplementation("com.linkedin.dexmaker:dexmaker-mockito-inline:2.28.1")
-//    androidTestImplementation("com.linkedin.dexmaker:dexmaker-mockito:2.28.1")
-
-//    androidTestImplementation("org.mockito:mockito-core:3.12.4")
-//    androidTestImplementation("org.mockito:mockito-android:3.12.4")
-//    androidTestImplementation("org.mockito:mockito-inline:3.12.4")
 }
 
